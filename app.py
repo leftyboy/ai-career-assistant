1355105b33254272cec38a51193b7216d85a05ff
leftyboy <adil.yada@gmail.com>
2026-02-23 12:09:47 +0100

    Ajout de la fonction push

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ import os
 import requests
 from pypdf import PdfReader
 import gradio as gr
+
+load_dotenv(override=True)
+
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
